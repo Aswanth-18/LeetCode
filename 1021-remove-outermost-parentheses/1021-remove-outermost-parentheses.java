fad54f4f3f0b6ca9eb907954ed3514893ccae770
Aswanthamudan S <aswanthamudan12@gmail.com>
2026-09-23 14:21:06 +0530

    Time: 4 ms (56.32%), Space: 43.7 MB (40.91%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,21 +1,39 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        Stack<Character> st = new Stack<>();
-        StringBuilder innerPara = new StringBuilder();
+        // Stack<Character> st = new Stack<>();
+        // StringBuilder innerPara = new StringBuilder();
 
-        for(int i = 0 ;i < s.length() ; i++){
+        // for(int i = 0 ;i < s.length() ; i++){
+        //     char ch = s.charAt(i);
+        //     if(ch == '('){
+        //         st.push(ch);
+        //         if(st.size()>1) innerPara.append(ch);
+        //     }
+        //     else{
+        //         if(ch==')' && st.size()>1){   
+        //             innerPara.append(ch);
+        //         }
+        //          st.pop();
+        //     }
+        // }
+        // return innerPara.toString();
+        int inner = 0;
+        StringBuilder para = new StringBuilder();
+
+        for(int i=0 ;i<s.length();i++){
             char ch = s.charAt(i);
-            if(ch == '('){
-                st.push(ch);
-                if(st.size()>1) innerPara.append(ch);
+            if(ch=='('){
+                inner++;
+
+                if(inner>1) para.append(ch);
             }
             else{
-                if(ch==')' && st.size()>1){   
-                    innerPara.append(ch);
+               if(ch==')' && inner>1){   
+                    para.append(ch);
                 }
-                 st.pop();
+                inner--;
             }
         }
-        return innerPara.toString();
+        return para.toString();
     }
 }
